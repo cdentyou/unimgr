a6045daa6e01a721e1667a2391f44fe73b9484b2
Alex Feigin <Alex.Feigin@hpe.com>
2016-04-29 16:44:15 +0200

    logging bind and unbind of Driver services

--- impl/src/main/java/org/mef/nrp/impl/ActivationDriverRepoService.java
+++ impl/src/main/java/org/mef/nrp/impl/ActivationDriverRepoService.java
@@ -4,7 +4,8 @@ import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.MountPointService;
 import org.opendaylight.controller.sal.binding.api.BindingAwareService;
 import org.opendaylight.yang.gen.v1.uri.onf.coremodel.corenetworkmodule.objectclasses.rev160413.GFcPort;
-import org.opendaylight.yang.gen.v1.uri.onf.coremodel.corenetworkmodule.objectclasses.rev160413.GForwardingConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,23 +14,29 @@ import java.util.List;
 import java.util.stream.Stream;
 
 /**
- *
- *
  * @author alex.feigin@hpe.com
  */
-    public class ActivationDriverRepoService implements BindingAwareService {
-
+public class ActivationDriverRepoService implements BindingAwareService {
+    private static final Logger LOG = LoggerFactory.getLogger(ActivationDriverRepoService.class);
     private static DataBroker dataBroker;
     private static MountPointService mountService;
-//    private final L2vpnXconnectDriverBuilder builder;
 
     private static List<ActivationDriverBuilder> builders = Collections.synchronizedList(new ArrayList<>());
 
     public void bindBuilder(ActivationDriverBuilder builder) {
+        if (builder == null) {
+            return;
+        }
+        LOG.info("ActivationDriverRepoService.bindBuilder got [{}] instance", builder.getClass().getSimpleName());
         builders.add(builder);
     }
 
     public void unbindBuilder(ActivationDriverBuilder builder) {
+        if (builder==null)
+        {
+            return;
+        }
+        LOG.info("ActivationDriverRepoService.unbindBuilder got [{}] instance", builder.getClass().getSimpleName());
         builders.remove(builder);
     }
 
