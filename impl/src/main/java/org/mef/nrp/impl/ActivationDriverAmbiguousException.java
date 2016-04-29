f0b023380428bcd1c2fd55d8a50285b848251be2
Alex Feigin <Alex.Feigin@hpe.com>
2016-04-29 10:40:21 +0200

    mocked up repo service holds all ActivationDriverBuilders and finds one that works for the port and fwding construct you need. service is not yet exported and populated, hopefully next commit

---
+++ impl/src/main/java/org/mef/nrp/impl/ActivationDriverAmbiguousException.java
@@ -0,0 +1,7 @@
+package org.mef.nrp.impl;
+
+/**
+ * Created by mef on 29/04/16.
+ */
+public class ActivationDriverAmbiguousException extends RuntimeException {
+}
---
+++ impl/src/main/java/org/mef/nrp/impl/ActivationDriverRepoService.java
@@ -0,0 +1,71 @@
+package org.mef.nrp.impl;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.MountPointService;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker;
+import org.opendaylight.controller.sal.binding.api.BindingAwareConsumer;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.l2vpn.cfg.rev151109.l2vpn.AutoDiscoveryBuilder;
+import org.opendaylight.yang.gen.v1.uri.onf.coremodel.corenetworkmodule.objectclasses.rev160413.GFcPort;
+import org.opendaylight.yang.gen.v1.uri.onf.coremodel.corenetworkmodule.objectclasses.rev160413.GForwardingConstruct;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Remo service mockup - soon a real one
+ *
+ * @author alex.feigin@hpe.com
+ */
+public class ActivationDriverRepoService implements IActivationDriverRepoService, BindingAwareConsumer {
+
+    private static DataBroker dataBroker;
+    private static MountPointService mountService;
+//    private final L2vpnXconnectDriverBuilder builder;
+
+    private static List<ActivationDriverBuilder> builders = Collections.synchronizedList(new ArrayList<>());
+
+    @Override
+    public void bindBuilder(ActivationDriverBuilder builder) {
+        builders.add(builder);
+    }
+
+    @Override
+    public void unbindBuilder(ActivationDriverBuilder builder) {
+        builders.remove(builder);
+    }
+
+    //    public static void initialize(BindingAwareBroker.ProviderContext session)  {
+//
+//        DataBroker dataBroker = session.getSALService(DataBroker.class);
+//        MountPointService mountService = session.getSALService(MountPointService.class);
+//
+//        if(instance != null) throw new IllegalStateException("already initialized");
+//        instance = new ActivationDriverRepoService(dataBroker, mountService);
+//    }
+
+
+    @Override
+    public ActivationDriver getBuilder(GFcPort port, GForwardingConstruct context) {
+        Stream<ActivationDriver> s = Arrays.stream(builders.toArray(new ActivationDriverBuilder[0]))//
+                .map(x -> x.driverFor(port, context))//
+                .filter(x -> x.isPresent())//
+                .map(x -> x.get());
+        if (s.count() > 1) {
+            throw new ActivationDriverAmbiguousException();
+        }
+        if (s.count() == 0) {
+            throw new ActivationDriverNotFoundException();
+        }
+        return s.findFirst().get();
+
+    }
+
+    @Override
+    public void onSessionInitialized(BindingAwareBroker.ConsumerContext session) {
+        DataBroker dataBroker = session.getSALService(DataBroker.class);
+        MountPointService mountService = session.getSALService(MountPointService.class);
+    }
+}
