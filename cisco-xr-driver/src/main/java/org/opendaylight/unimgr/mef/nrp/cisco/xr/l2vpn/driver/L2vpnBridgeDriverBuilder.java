27828b12d56b3a45f4d45f1e6c6e56a3ccc9664c
Krzysztof Bijakowski <krzysztof.bijakowski@amartus.com>
2016-08-29 18:31:49 +0200

    Capabilities service and drivers ability of devices configuration verification
    Change-Id: I595067a1840454773bf785f40eeefedf4f97fa89

--- cisco-xr-driver/src/main/java/org/opendaylight/unimgr/mef/nrp/cisco/xr/l2vpn/driver/L2vpnBridgeDriverBuilder.java
+++ cisco-xr-driver/src/main/java/org/opendaylight/unimgr/mef/nrp/cisco/xr/l2vpn/driver/L2vpnBridgeDriverBuilder.java
@@ -8,15 +8,21 @@
 
 package org.opendaylight.unimgr.mef.nrp.cisco.xr.l2vpn.driver;
 
-import java.util.Optional;
-
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.MountPointService;
 import org.opendaylight.unimgr.mef.nrp.api.ActivationDriver;
 import org.opendaylight.unimgr.mef.nrp.api.ActivationDriverBuilder;
+import org.opendaylight.unimgr.mef.nrp.cisco.xr.common.util.CapabilitiesService;
 import org.opendaylight.unimgr.mef.nrp.cisco.xr.l2vpn.activator.L2vpnBridgeActivator;
 import org.opendaylight.yang.gen.v1.urn.onf.core.network.module.rev160630.forwarding.constructs.ForwardingConstruct;
 import org.opendaylight.yang.gen.v1.urn.onf.core.network.module.rev160630.g_forwardingconstruct.FcPort;
+
+import java.util.Optional;
+
+import static org.opendaylight.unimgr.mef.nrp.cisco.xr.common.util.CapabilitiesService.Capability.Mode.AND;
+import static org.opendaylight.unimgr.mef.nrp.cisco.xr.common.util.CapabilitiesService.NodeContext.NodeCapability.NETCONF;
+import static org.opendaylight.unimgr.mef.nrp.cisco.xr.common.util.CapabilitiesService.NodeContext.NodeCapability.NETCONF_CISCO_IOX_IFMGR;
+import static org.opendaylight.unimgr.mef.nrp.cisco.xr.common.util.CapabilitiesService.NodeContext.NodeCapability.NETCONF_CISCO_IOX_L2VPN;
 
 /**
  * Provides drivers for binding two ports on the same node.
@@ -24,11 +30,14 @@ import org.opendaylight.yang.gen.v1.urn.onf.core.network.module.rev160630.g_forw
  */
 public class L2vpnBridgeDriverBuilder implements ActivationDriverBuilder {
 
+    private final DataBroker dataBroker;
+
     private L2vpnBridgeActivator activator;
 
     private static final String GROUP_NAME = "local";
 
     public L2vpnBridgeDriverBuilder(DataBroker dataBroker, MountPointService mountPointService) {
+        this.dataBroker = dataBroker;
         activator = new L2vpnBridgeActivator(dataBroker, mountPointService);
     }
 
@@ -39,7 +48,14 @@ public class L2vpnBridgeDriverBuilder implements ActivationDriverBuilder {
 
     @Override
     public Optional<ActivationDriver> driverFor(FcPort aPort, FcPort zPort, BuilderContext context) {
-        return Optional.of(getDriver());
+        CapabilitiesService capabilitiesService = new CapabilitiesService(dataBroker);
+
+        if (capabilitiesService.nodeByPort(aPort).isSupporting(AND, NETCONF, NETCONF_CISCO_IOX_IFMGR, NETCONF_CISCO_IOX_L2VPN) &&
+            capabilitiesService.nodeByPort(zPort).isSupporting(AND, NETCONF, NETCONF_CISCO_IOX_IFMGR, NETCONF_CISCO_IOX_L2VPN)) {
+                return Optional.of(getDriver());
+        }
+
+        return Optional.empty();
     }
 
     protected ActivationDriver getDriver() {
